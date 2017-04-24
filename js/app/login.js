d25137eaaebfc87ac1fe768b3eaca559c694914e
xiaowuge007 <864820966@qq.com>
2017-04-24 22:28:57 +0800

    change js

--- js/app/login.js
+++ js/app/login.js
@@ -8,6 +8,7 @@ $(window).load(function(){
         $("#_enter ._form").hide();
         $("#_enter ._form").eq($(this).index()).show();
     })
+    var $a = 123;
     var $btn = true;
     $("._ck1").click(function(){
         if($btn){
